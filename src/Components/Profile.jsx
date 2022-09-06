be34b5f62fe9106bce83eed4de213727c6446408
aramstrange <aramstrange@gmail.com>
2022-09-06 17:50:14 +0400

    Lesson13

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,30 @@
+import React, { Profiler } from 'react';
+
+const Profile = () => {
+  return (
+    <div class='content'>
+      <div>
+        <img src='https://www.tripsavvy.com/thmb/kjKaebyEA1bGWwICjgnZ0NZmKE4=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/beach-5b59c9b7c9e77c004b3e0ff0.jpg'></img>
+      </div>
+      <div>
+        avatar + description
+        {/* <img src='https://cdn.britannica.com/91/181391-050-1DA18304/cat-toes-paw-number-paws-tiger-tabby.jpg'></img> */}
+      </div>
+      <div>
+        My posts
+        <div>
+          New Post
+        </div>
+        <div>
+          Post1
+        </div>
+        <div>
+          Post2
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Profile;
